78f63da00222fc1c319bc658ac2fdbd478518060
Angus Day <gp002901@student.reading.ac.uk>
2023-11-23 23:36:22 +0000

    app.js error message added

--- app.js
+++ app.js
@@ -6,14 +6,30 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+  try {
+    const stockSymbols = await stocks.getStocks()
+    res.send({ stockSymbols })
+  } 
+  /** Returns a meaningful error message
+  * when the stock data cannot be retrieved
+  */
+  catch (error) {
+    console.error('Error while retrieving stock data', error.message);
+    res.status(500).send({ error: error.message });
+  }
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
+  try {
+    const { params: { symbol } } = req
+    const data = await stocks.getStockPoints(symbol, new Date())
+    res.send(data)
+  }
+  // Returns a meaningful error message
+  catch (error) {
+    console.error('Error while retrieving stock data', error.message);
+    res.status(500).send({ error: error.message });
+  }
 })
 
 app.listen(3000, () => console.log('Server is running!'))
